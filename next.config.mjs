18d6f306777fc8787c9cba531823740bdb006d7d
waqas837 <waqaskhanbughlani1124@gmail.com>
2025-01-06 21:10:38 +0500

    config file update

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,17 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "localhost",
+        hostname: "vidmate.drawsketch.co",
       },
     ],
   },
 };
 
-// const withPWA = nextPWA({
-//   dest: "public", // Destination directory for the PWA files
-//   disable: process.env.NODE_ENV === "development", // Disable PWA in
-//   skipWaiting: true, // Skip waiting for service worker activation
-// });
+const withPWA = nextPWA({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in
+  skipWaiting: true, // Skip waiting for service worker activation
+});
 
-// export default withPWA(nextConfig);
-export default nextConfig;
+export default withPWA(nextConfig);
+// export default nextConfig;
